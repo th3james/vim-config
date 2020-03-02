ad4fdebac01eba70802aaaed8c75844e29b9bf17
James Cox-Morton <james.cox-morton@arm.com>
2020-03-02 13:24:52 +0000

    Add git merge and rebase aliases

--- config/fish/functions/aliases.fish
+++ config/fish/functions/aliases.fish
@@ -5,8 +5,9 @@ alias tmux "env TERM=screen-256color tmux"
 # Git #
 alias ga 'git add'
 alias gst 'git status'
-alias gcb 'git checkout -b'
 alias gcm 'git checkout master'
 alias gco 'git checkout'
 alias gd 'git diff'
 alias gc 'git commit -v'
+alias gfm 'git fetch; git merge --ff-only'
+alias gfr 'git fetch; git rebase'
