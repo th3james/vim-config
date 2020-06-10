7e8aefe76117023ea375f508cc24eb2cfb5eee45
James Cox-Morton <james.cox-morton@arm.com>
2020-06-10 12:25:28 +0100

    Add pyenv and rust

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,12 @@ git config --global core.editor vim
 
 direnv hook fish | source
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
+
+# pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+pyenv init - | source
