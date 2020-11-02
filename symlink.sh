dd8dd6fc725190c67cb3496583458ebb85b17c1b
James Cox-Morton <th3james@fastmail.fm>
2020-11-02 12:28:05 +0000

    Tidy up git config

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env fish
 
-ln -s ~/.sys-config/gitignore ~/.gitignore
+rm -f ~/.gitignore
+ln -s ~/src/sys-config/gitignore ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
 
-ln -s ~/.sys-config/tmux.conf ~/.tmux.conf
+rm -f ~/.tmux.conf
+ln -s ~/src/sys-config/tmux.conf ~/.tmux.conf
 
 rm -rf ~/.config
 ln -fs ~/src/sys-config/config/  ~/.config
