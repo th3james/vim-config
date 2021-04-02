bdb22cb4669e0a172c3fa390997a036d260499f5
th3james <th3james@katsuragi.local>
2021-04-02 20:46:02 +0100

    Add a --merged option to delete-git-branches

--- config/fish/functions/delete-git-branches.fish
+++ config/fish/functions/delete-git-branches.fish
@@ -1,14 +1,22 @@
 # Inspired by
 # https://seb.jambor.dev/posts/improving-shell-workflows-with-fzf/
-function delete-git-branches -d 'Delete git branches by name' -a arg_1 arg_b
+function delete-git-branches -d 'Delete git branches by name'
+  argparse 'm/merged' -- $argv
+  or return
+
+  set -l git_command 'git branch'
+  if string length -q --  $_flag_merged
+    set git_command $git_command ' --merged'
+  end
+
   set -l branches_to_delete (
-    git branch | 
-    rg -v '\*' |
+    eval $git_command |
+    rg -v '^\*' |
     cut -c 3- |
     fzf --multi --preview="git log {} --"
   )
   if string length -q -- $branches_to_delete
-    git branch --d "$branches_to_delete"
+    git branch --d $branches_to_delete
   else
     echo "no branches given - exiting"
   end
